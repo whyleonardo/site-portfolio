36403683a64bffb32da87e5a3bf10fc43102eb5b
whyleonardo <pleasefloyd73@gmail.com>
2022-06-29 19:56:56 -0300

    feat: projects section

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,8 @@ const body = document.querySelector('body')
 const header = document.getElementById('header')
 const hamburguer = document.querySelector('.hamburger')
 const menu = document.querySelector('.menu')
+const cards = document.querySelectorAll('.card.hidden')
+const expandButton = document.querySelector('.expand-projects')
 
 const openMenu = () => {
   hamburguer.classList.toggle('is-active')
@@ -16,7 +18,7 @@ const openMenu = () => {
   }
 }
 
-const headerBlur = () => {
+const removeHeader = () => {
   if (window.scrollY > 20) {
     header.classList.add('scroll')
   } else {
@@ -24,12 +26,24 @@ const headerBlur = () => {
   }
 }
 
-// Close menu when click on any item of list.
+// Close menu when click on any item of menu list.
 const links = document.querySelectorAll('.menu-list ul li a')
 for (const link of links) {
   link.addEventListener('click', () => {
     menu.classList.remove('menu-active')
     hamburguer.classList.remove('is-active')
     body.style.overflow = 'auto'
+  })
+}
+
+// expand project cards
+const expandCards = () => {
+  cards.forEach(card => {
+    card.classList.toggle('hidden')
+    if (card.classList.contains('hidden')) {
+      expandButton.textContent = 'Ver mais'
+    } else {
+      expandButton.textContent = 'Ver menos'
+    }
   })
 }
--- src/js/scroll.js
+++ src/js/scroll.js
@@ -5,15 +5,17 @@ ScrollReveal().reveal('body', { delay: 50 })
 // Scroll Reveal
 const scrollReveal = ScrollReveal({
   origin: 'top',
-  distance: '150px',
+  distance: '120px',
   duration: 800,
   reset: true
 })
 
 scrollReveal.reveal(
-  `#home .image, #home .main-wrapper h3, #home .main-wrapper h1,
-   #about .text, #about .image,
-   #services header, #services .card,
+  `#home .image, #home .home-wrapper h3, #home .home-wrapper h1,
+   #about .about-wrapper img, #about .about-wrapper h2,
+   #about .about-wrapper p, #about .about-wrapper .paragraph_2,
+   #projects .projects-wrapper h2, #projects .projects-wrapper ,
+   
    #testimonials header, #testimonials .testimonials,
    #contact .text, #contact .links,
    footer .brand, footer .social
