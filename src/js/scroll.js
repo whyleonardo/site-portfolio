1cbd4557da01174f55a950b14029a9004efb680e
whyleonardo <pleasefloyd73@gmail.com>
2022-06-30 16:39:49 -0300

    feat: footer and media queries

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -5,7 +5,7 @@ ScrollReveal().reveal('body', { delay: 50 })
 // Scroll Reveal
 const scrollReveal = ScrollReveal({
   origin: 'top',
-  distance: '120px',
+  distance: '50px',
   duration: 800,
   reset: true
 })
@@ -16,8 +16,6 @@ scrollReveal.reveal(
    #about .about-wrapper p, #about .about-wrapper .paragraph_2,
    #projects .projects-wrapper h2, #projects .projects-wrapper,
    #skills h2, #skills .skills-wrapper .skill,
-   #testimonials header, #testimonials .testimonials,
-   #contact .text, #contact .links,
    footer .brand, footer .social
   `,
   { interval: 100 }
